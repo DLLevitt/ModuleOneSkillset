e40c47351944375657cb17a67b882d09e89f1b82
DLLevitt <dlevitt@lakelandcollege.edu>
2023-09-03 13:32:03 -0500

    Second Edition

--- ModuleOneSkillset.py
+++ ModuleOneSkillset.py
@@ -1,12 +1,23 @@
 import os
+import time
 import datetime
 
 eventList = []
 
-for i in eventList:
-   print(i)
+# list to store the people in, and have the program print a message 
+# to invite the people to your event.  Be sure the invite message 
+# prints the person, and the invite, including the name of the event 
+# you are inviting them to
+def printEventList():
+  print()
+  
+  for invitee, addInvitee in enumerate(eventList, start = 1):
+    print(f"{invitee}.\t {addInvitee}")
 
+# While loop for adding event invites
 while True:
+    # listMenu - would you like to add a name to the beginning, middle, or end of the list
+
     addInvitee = input("Invitee's first and last name: ")
 
     if(addInvitee) not in eventList:
@@ -18,5 +29,6 @@ while True:
        print("Invitee already added to Event")
        print()
     
+
 
 
